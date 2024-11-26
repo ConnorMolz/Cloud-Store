68959993038adb8816c72e9840e33e7e7266a10c
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-11-26 12:14:53 +0100

    Fix layer 8 Problem

--- Cloud_Store/Models/ViewModels/LoginViewModel.cs
+++ Cloud_Store/Models/ViewModels/LoginViewModel.cs
@@ -10,7 +10,6 @@ public class LoginViewModel
     [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a password.")]
     public string? Password { get; set; }
     
-    [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a password.")]
     public string? ConfirmPassword { get; set; }
     
     
